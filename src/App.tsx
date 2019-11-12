a31ebef4d330f16ce07a990a47254193cf4998ce
conorburke <cjburke89@gmail.com>
2019-11-12 13:03:51 +0900

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Particles from 'react-particles-js';
+
+import './App.css';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+        <Particles />
+    </div>
+  );
+}
+
+export default App;
